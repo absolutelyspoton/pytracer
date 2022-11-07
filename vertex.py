f838884dd632c5dc5dc4ba1d1bba4a84f9c410ab
absolutelyspoton <dominic.jm.williams@gmail.com>
2022-11-07 10:35:42 +0000

    pydantic version

--- vertex.py
+++ vertex.py
@@ -3,47 +3,42 @@
 # 
 # Classes for modelling vertices
 
+from pydantic import BaseModel
+from typing import List
+from devtools import debug
 
-class vertex:
+class vertex(BaseModel):
+    x:float
+    y:float
+    z:float
 
     def normal(self,normal):
         self.normal = normal
 
-    def __init__(self,x=0,y=0,z=0):
-        self.x = x
-        self.y = y
-        self.z = z
-
 class vertices:
+    vertex_list: List = []
 
-    vertex_list = []
-
-    def add_vertex(self,v):
+    def add_vertex(self,v:vertex):
         self.vertex_list.append(v)
 
     def vertex_count(self):
         return(len(self.vertex_list))
 
-    def __init__(self):
-        return
-
-
 if __name__ == '__main__':
 
-    print(__file__)
-
-    v1 = vertex(1,2,3)
-    v2 = vertex(2,3,4)
+    v1 = vertex(x=1.0,y=2.0,z=3.0)
+    v2 = vertex(x=2.0,y=3.0,z=4.0)
     
     vl = vertices() 
 
     vl.add_vertex(v1)
     vl.add_vertex(v2)
 
-    print(vl.vertex_count())
-    print(vl.vertex_list[0].x)
+    debug(vl.vertex_count())
+    debug(vl.vertex_list[0].x)
 
 
 else :
-    print(__name__)
+    debug(__file__)
+    debug(__name__)
         
