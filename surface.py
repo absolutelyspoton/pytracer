88b1d7526f719b5caca3340a065f06e2e68a9a3b
absolutelyspoton <dominic.jm.williams@gmail.com>
2022-11-08 08:09:21 +0000

    pydantic added

--- surface.py
+++ surface.py
@@ -2,46 +2,46 @@
 # Date created: 10 Aug 2022
 # 
 # Classes for surfaces ( collection of faces ( list of indexes to vertices ))
- 
+
+from pydantic import BaseModel
+from typing import List
+from devtools import debug
+
 class face:
 
-    # list of indexes to vertices
-    vertex_list = []
-    normal = []
+    vertex_list: List = []
+    normal: List = []
 
     # add index to the index list for this face
-    def add_face_index(self,vl):
+    def add_face_index(self,vl:List):
         self.vertex_list = vl
 
     # return the number of vertex indices for this face
     def face_index_count(self):
-        return(len(self.vertex_list))
+        return(len(self.vertex_list)) 
 
     def calcNormal(self,v1,v2,v3):
             
         # First calculate colinnear vectors
         vect1 = [v3[0]-v2[0],v3[1]-v2[1],v3[2]-v2[2]]
         vect2 = [v2[0]-v1[0],v2[1]-v1[1],v2[2]-v1[2]]
-        # Calculate the norm
+
+        # Calculate the normal
         x = vect1[1] * vect2[2] - vect1[2] * vect2[1]
         y = vect1[2] * vect2[0] - vect1[0] * vect2[2]
         z = vect1[0] * vect2[1] - vect1[1] * vect2[0]
+
         # Store it
         self.normal = [x,y,z]
 
-    # initialise class
-    def __init__(self):
-        return None
-
 class surface:
 
-    surface_list = []
+    surface_list: List = []
 
-    def add_face(self,face):
-        self.surface_list.append(face)
+    def add_face(self,f:face):
+        self.surface_list.append(f)
 
     def surface_count(self):
         return(len(self.surface_list))
 
-    def __init__(self):
-        return
+
