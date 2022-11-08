5ad4134edeb7729d20de59a4ea2bcfabc2936152
absolutelyspoton <dominic.jm.williams@gmail.com>
2022-11-08 08:09:34 +0000

    fixed linting errors

--- tests/test_vertex.py
+++ tests/test_vertex.py
@@ -6,21 +6,21 @@
 import vertex 
 
 def test_create_vertex_object1():
-    v = vertex.vertex(0,0,0)
+    v = vertex.vertex(x=0.0,y=0.0,z=0.0)
     assert v.x ==0
     assert v.y == 0
     assert v.x == 0
 
 
 def test_create_vertex_object2():    
-    v = vertex.vertex()
+    v = vertex.vertex(x=0.0,y=0.0,z=0.0)
     assert v.x ==0
     assert v.y == 0
     assert v.x == 0
 
 def test_create_vertices_object():
     l = vertex.vertices()
-    v = vertex.vertex(0,0,0)
+    v = vertex.vertex(x=0.0,y=0.0,z=0.0)
     assert l.vertex_count() == 0
     l.add_vertex(v)
     assert l.vertex_count() == 1
