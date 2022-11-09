e87856fe0df2c9d04ac30f71e586409e5bf30bd3
absolutelyspoton <dominic.jm.williams@gmail.com>
2022-11-09 10:21:15 +0000

    updates to ping api

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
 from enum import Enum
+import time
+import requests
+from typing import List
 
 app = FastAPI()
 
 DEV_MONGODB_ADDRESS = 'mongodb://localhost:27017/'
+GOOGLE_ADDRESS = 'http://www.google.com'
 
 class PingTypes(str, Enum):
     ws = "webserver"
@@ -12,16 +16,22 @@ class PingTypes(str, Enum):
     g = "google"
 
 @app.get('/ping/{pingtype}')
-async def ping(pingtype:PingTypes):
+async def ping(pingtype:PingTypes) -> float:
+
+    t:float = time.perf_counter()
 
     if pingtype == PingTypes.db:
-        print('db ping')
+        client = MongoClient(DEV_MONGODB_ADDRESS)
+        client.close()
+    elif pingtype == PingTypes.ws:
+        pass
+    elif pingtype == PingTypes.g:
+        r = requests.get(GOOGLE_ADDRESS)
+        print(r.text)
     else:
         print(pingtype)
 
-    return
-
-
+    return (time.perf_counter() - t)
 
 @app.get('/db/{database}/{table}/{id}')
 async def get_data(database:str,table:str,id:int):
@@ -32,7 +42,8 @@ async def get_data(database:str,table:str,id:int):
         filter = {'id':id}
 
     result = client[database][table].find(filter=filter)
-    r = []
+
+    r:List = []
     for n in result:
         r.append(n)
 
