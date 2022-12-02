df456ea97cc8ef6581e96a7d4877ad3ab3e4d94c
absolutelyspoton <dominic.jm.williams@gmail.com>
2022-12-02 15:23:23 +0000

    cleanup

--- tests/test_vertex.py
+++ tests/test_vertex.py
@@ -5,22 +5,15 @@
 
 import vertex 
 
-def test_create_vertex_object1():
-    v = vertex.vertex(x=0.0,y=0.0,z=0.0)
-    assert v.wx ==0
-    assert v.wy == 0
-    assert v.wx == 0
-
-
-def test_create_vertex_object2():    
-    v = vertex.vertex(x=0.0,y=0.0,z=0.0)
-    assert v.wx ==0
-    assert v.wy == 0
-    assert v.wx == 0
+def test_create_vertex_object():
+    v = vertex.vertex(x_world=0.0,y_world=1.0,z_world=0.1)
+    assert v.x_world == 0
+    assert v.y_world == 1
+    assert v.z_world == 0.1
 
 def test_create_vertices_object():
     l = vertex.vertices()
-    v = vertex.vertex(wx=0.0,wy=0.0,wz=0.0)
+    v = vertex.vertex(x_world=0.0,y_world=0.0,z_world=0.0)
     assert l.vertex_count() == 0
     l.add_vertex(v)
     assert l.vertex_count() == 1
